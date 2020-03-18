5683eaed06bd4a53828b8b7d78c382806c39187a
JWeez <jwellstx@gmail.com>
2020-03-17 22:45:01 -0500

    checkers for num of song votes, current songs, new api token request, api calls for new songs, and check for winners

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,105 +9,121 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-// var song1votes = 0;
-// var song2votes = 0;
-var offset = 0;
-
 var db = firebase.database();
+
+// sample authorization for client verification -> ignore this
+// https://accounts.spotify.com/en/authorize?client_id=aa373cd188254322a46c8e258cd404a9&redirect_uri=https:%2F%2Fjwellstx.github.io%2FProject1%2Fcallback&response_type=token&state=123
+
+
+var offset = 0;
+var db = firebase.database();
+
+// this just checks if anyone has already voted, if not set initial values to 0
+// also, check if 2 songs already exist, if so, use those.  If none defined, generate new ones
 db.ref("spotify").once("value", snapshot => {
     var dbRef = snapshot.val();
 
-    if (dbRef.song1votes) {
+    // checks for song1votes existence
+    if (snapshot.child("song1votes").exists()) {
         song1votes = dbRef.song1votes;
     }
     else {
         song1votes = 0;
     }
 
-    if (dbRef.song2votes) {
+    // checks for song2votes existence
+    if (snapshot.child("song2votes").exists()) {
         song2votes = dbRef.song2votes;
     }
     else {
         song2votes = 0;
     }
+
+    // check to make sure boths songs are selected, if not generate new songs
+    if (!snapshot.child("song1").exists() || !snapshot.child("song2").exists()) {
+        newSongs();
+    }
+    else {
+        $("#song1").attr("src", dbRef.song1);
+        $("#song2").attr("src", dbRef.song2);
+    }
 });
 
-var songurl = "https://open.spotify.com/embed/track/";
-// var offset = Math.floor(Math.random() * 30);
 
-
-// only run ajax call once timer runs out
-newSongs(); // just to refresh for now
-var timer = setInterval(newSongs, 10 * 1000);
 
 function newSongs() {
-    var x = 10;
-    var timer2 = setInterval(function () { 
-        console.log(x);
-        x--;
-
-        $("#timer").html(x);
-
-        if (x === 0) {
-            db.ref("spotify").once("value", snapshot => { 
-                var dbRef = snapshot.val();
-                console.log(dbRef.song1votes, dbRef.song2votes);
-                if (parseInt(dbRef.song1votes) > parseInt(dbRef.song2votes)) {
-                    console.log("Song 1 Wins!!");
-                    $("#winners").prepend("Song 1 Won!<br>");
-                }
-                else if (parseInt(dbRef.song2votes) > parseInt(dbRef.song1votes)) {
-                    console.log("Song 2 Wins!!");
-                    $("#winners").prepend("Song 2 Won!<br>");
-                }
-                else if (parseInt(dbRef.song2votes) === parseInt(dbRef.song1votes)) {
-                    console.log("Its a tie!! overtime?");
-                    $("#winners").prepend("It's a tie! Everyone Wins!<br>")
-                }
-            });
-
-           
-            clearInterval(timer2);
-            db.ref("spotify").update({
-                song1votes: 0,
-                song2votes: 0,
-            });
-        }
-    }, 1000);
+    var encoded = "";  // used for base64 to ascii conversion for api key
+    var access_token = "";   // access token to call spotify api 
+    var songurl = "https://open.spotify.com/embed/track/";      // url to get song track
 
 
-    console.log("picking new songs");
-    $.ajax({
-        url: 'https://api.spotify.com/v1/search?q=muse&type=track&limit=1&offset=' + offset,
-        headers: {
-            'Authorization': 'Bearer ' + 'BQBNtET2QDLqxr7smC8h9YAp1Nl4H5XpRwgEQXWfzF3_Q8sejDNcMTwloeYkNNXeocXn3AcHF-YCNNIGZrKiKCzJtyNbUpuCvNsul4_BXBeoY7zOzAU7ov3iVzvNpJKHuj_JWnNURjhxGN-YLxxfyw'
-        },
-        success: function (response) {
-            $("#song1").attr("src", songurl + response.tracks.items[0].id);
-        }
-    }).then(function (respone) {
-        // var offset = Math.floor(Math.random() * 30);
+    // bunch of chained calls
+    db.ref().once("value", snapshot => {
+        // when new songs are needed, get the id and pass from DB and encode it for api call
+        var dbRef = snapshot.val();
+        encoded = btoa(dbRef.notTheClientId + ':' + dbRef.notTheClientShhhh);
+    }).then(function () {
+        // Do the first API call to api/token endpoint to get authorization/access token to do api calls
         $.ajax({
-            url: 'https://api.spotify.com/v1/search?q=post%20malone&type=track&limit=1&offset=' + offset,
-            headers: {
-                'Authorization': 'Bearer ' + 'BQBNtET2QDLqxr7smC8h9YAp1Nl4H5XpRwgEQXWfzF3_Q8sejDNcMTwloeYkNNXeocXn3AcHF-YCNNIGZrKiKCzJtyNbUpuCvNsul4_BXBeoY7zOzAU7ov3iVzvNpJKHuj_JWnNURjhxGN-YLxxfyw'
+            url: 'https://accounts.spotify.com/api/token',
+            type: 'POST',
+            data: {
+                grant_type: "client_credentials",
+                'Content-Type': 'application/x-www-form-urlencoded'
             },
-            success: function (response) {
-                $("#song2").attr("src", songurl + response.tracks.items[0].id);
-            }
+            headers: {
+                Authorization: 'Basic ' + encoded
+            },
+            dataType: 'json'
+        }).then(function (response) {
+            // this function takes the access token and does a spotify api call to get a new song, then updated #song1
+            access_token = response.access_token;
+            $.ajax({
+                url: 'https://api.spotify.com/v1/search?q=muse&type=track&limit=1&offset=' + offset,
+                method: "GET",
+                headers: {
+                    'Authorization': 'Bearer ' + access_token
+                },
+                success: function (response) {
+                    // Update song1 with spotify song
+                    $("#song1").attr("src", songurl + response.tracks.items[0].id);
+                    // Update song1 url in DB so we can sync across users
+                    db.ref("spotify").update({
+                        song1: songurl + response.tracks.items[0].id,
+                    });
+                }
+            }).then(function (response) {
+                // same as previous function but for second song
+                $.ajax({
+                    url: 'https://api.spotify.com/v1/search?q=post%20malone&type=track&limit=1&offset=' + offset,
+                    method: "GET",
+                    headers: {
+                        'Authorization': 'Bearer ' + access_token
+                    },
+                    success: function (response) {
+                        $("#song2").attr("src", songurl + response.tracks.items[0].id);
+                        db.ref("spotify").update({
+                            song2: songurl + response.tracks.items[0].id,
+                        });
+                    }
+                });
+            }).then(function (response2) {
+                // increase offset by 1 to get a new song -> otherwise we pull the same song every time
+                offset++;
+            });
         });
-    }).then(function(response2){
-        offset++;
     });
 }
 
+// button to vote for song 1 - if pressed, update number of song1 votes then update DB
+// to reflect across users
 $("#votedsong1").on('click', function () {
     var voted = localStorage.getItem("hasvoted");
 
     if (voted) return;
     else {
         song1votes++;
+        // prevent users from voting multiple times
         // localStorage.setItem("hasvoted", true);
     }
     db.ref("spotify").update({
@@ -115,12 +131,16 @@ $("#votedsong1").on('click', function () {
     });
 
 });
+
+// button to vote for song 2 - if pressed, update number of song2 votes then update DB
+// to reflect across users
 $("#votedsong2").on('click', function () {
     var voted = localStorage.getItem("hasvoted");
 
     if (voted) return;
     else {
         song2votes++;
+        // prevent users from voting multiple times
         // localStorage.setItem("hasvoted", true);
     }
     db.ref("spotify").update({
@@ -128,6 +148,9 @@ $("#votedsong2").on('click', function () {
     });
 });
 
+// Update num of votes for each song on webpage when someone clicks the vote button.
+// also if song votes for either one hit 15, announce winner then call newSongs to get new songs
+// then reset vote count to 0
 db.ref("spotify").on("value", snapshot => {
     if (snapshot.child("song1votes").exists()) {
         $("#song1votes").html(snapshot.val().song1votes);
@@ -136,5 +159,22 @@ db.ref("spotify").on("value", snapshot => {
     if (snapshot.child("song2votes").exists()) {
         $("#song2votes").html(snapshot.val().song2votes);
         song2votes = snapshot.val().song2votes;
+    }
+
+    if (song1votes == 15) {
+        $("#winners").prepend("Song 1 won!!<br>");
+        db.ref("spotify").update({
+            song1votes: 0,
+            song2votes: 0,
+        });
+        newSongs();
+    }
+    else if (song2votes == 15) {
+        $("#winners").prepend("Song 2 won!!<br>");
+        db.ref("spotify").update({
+            song1votes: 0,
+            song2votes: 0,
+        });
+        newSongs();
     }
 });
