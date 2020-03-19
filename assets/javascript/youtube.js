9b7f0cb86a7e3f6b5eefc9e3b1256e247946a3e3
Karina Titov <karinasul1995@gmail.com>
2020-03-19 15:55:19 -0500

    got a response from youtube api

--- assets/javascript/youtube.js
+++ assets/javascript/youtube.js
@@ -1,22 +1,15 @@
-  function loadClient() {
-    gapi.client.setApiKey("AIzaSyD_hRyR6sbL4dXjrTlyjFNU4Z5uxR6C1Sw");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
-  }
-  // Make sure the client is loaded and sign-in is complete before calling this method.
-  function execute() {
-    return gapi.client.youtube.search.list({
-      "part": "snippet",
-      "channelType": "any",
-      "eventType": "completed",
-      "maxResults": 1,
-      "q": "surfing"
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
- 
+var queryUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&type=video&key=AIzaSyD_hRyR6sbL4dXjrTlyjFNU4Z5uxR6C1Sw' 
+// --header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
+// --header 'Accept: application/json' \
+// --compressed
+
+$.ajax({
+    url: queryUrl,
+    method: "Get"    
+}).done(function(response){
+    console.log(response);
+for(var i = 0; i < items.length; i++){
+    var video1 = $("<div>").response.item[i];
+    console.log(video1);
+    }
+})
