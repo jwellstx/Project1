a0a3535af11e4599f4fdca64335bb32f92c4ed9d
Karina Titov <karinasul1995@gmail.com>
2020-03-19 18:21:31 -0500

    random video is picked

--- assets/javascript/youtube.js
+++ assets/javascript/youtube.js
@@ -14,6 +14,9 @@ var firebaseConfig = {
   db.ref("youtube").once("value", snapshot => {
     var dbRef = snapshot.val();
 
+    var video1votes = 0;
+    var video2votes = 0;
+
     // checks for video1votes existence
     if (snapshot.child("video1votes").exists()) {
         video1votes = dbRef.video1votes;
@@ -38,22 +41,30 @@ var firebaseConfig = {
         $("#video1").attr("src", dbRef.video1);
         $("#video2").attr("src", dbRef.video2);
     }
+
+    db.ref("youtube").set({video1votes, video2votes});
 });
 
 
-
-var queryUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&type=video&key=AIzaSyD_hRyR6sbL4dXjrTlyjFNU4Z5uxR6C1Sw' 
+function newvideos(){
+var queryUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&type=video&key=AIzaSyD_hRyR6sbL4dXjrTlyjFNU4Z5uxR6C1Sw'; 
 // --header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
 // --header 'Accept: application/json' \
 // --compressed
-
+db.ref().once("value", snapshot => {
+    var dbRef = snapshot.val();
+}).then(function(){
 $.ajax({
     url: queryUrl,
     method: "Get"    
 }).done(function(response){
-    console.log(response);
-for(var i = 0; i < items.length; i++){
-    var video1 = $("<div>").response.item[i];
-    console.log(video1);
-    }
+   // console.log(response.items[Math.random() * response.items.length]);
+var pickVideo1 = response.items[Math.floor(Math.random() * response.items.length)].id.videoId;
+var pickVideo2 = response.items[Math.floor(Math.random() * response.items.length)].id.videoId;
+// console.log(pickVideo1);
+
+$("#player1").attr("src", "http://www.youtube.com/embed/" + pickVideo1);
+$("#player2").attr("src", "http://www.youtube.com/embed/" + pickVideo2);
 })
+})
+}
